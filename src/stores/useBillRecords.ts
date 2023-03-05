b054dc0e64c33803ec5f8621b3df92c972537370
j10c <blyb1739@gmail.com>
2023-03-05 16:29:56 +0800

    feat(input): track index by date on operators
    feat(input): add date tracker of record-list
    fix(input): fix insert record by date order issue

--- src/stores/useBillRecords.ts
+++ src/stores/useBillRecords.ts
@@ -3,10 +3,14 @@ import create from "zustand";
 import storage from "./storage";
 
 type BillRecordsState = {
-  list: Bill.BillRecord[]
-  addItem: (item: Bill.BillRecord) => void;
+  list: BillAPI.BillRecord[];
+  // TODO: handle retry
+  unsyncList: Array<BillAPI.BillRecord & { operateTye: string }>;
+  indexList: Array<BillAPI.DateIndex>;
+  addItem: (item: BillAPI.BillRecord) => void;
   removeItem: (uid: string) => boolean;
-  replaceItem: (uid: string, value: Omit<Bill.BillRecord, "uid">) => void;
+  updateItem: (uid: string, value: Omit<BillAPI.BillRecord, "uid">) => void;
+  updateIndex: (records: Array<BillAPI.BillRecord>, type?: "add" | "delete") => void;
 }
 
 export const useBillRecords = create<
@@ -15,13 +19,20 @@ export const useBillRecords = create<
 >(
   persist((set, get) => ({
     list: [],
+    unsyncList: [],
+    indexList: [],
     addItem: (item) => {
+      get().updateIndex([item]);
       const date = item.date;
       const [YEAR, MONTH, DAY] = date.split("-").map(item => parseInt(item));
       const list = get().list;
       let index = list.findIndex(item => {
         const [year, month, day] = item.date.split("-").map(item => parseInt(item));
-        if (year <= YEAR && month <= MONTH && day <= DAY)
+        if (year < YEAR) {
+          return true;
+        } else if (year === YEAR && month < MONTH) {
+          return true;
+        } else if (month === MONTH && day < DAY)
           return true;
       });
       if (index === -1) index = list.length;
@@ -33,17 +44,77 @@ export const useBillRecords = create<
       let isFound = false;
       set(state => {
         const index = state.list.findIndex(item => uid === item.uid);
-        if (index !== -1) isFound = true;
+        if (index !== -1){
+          state.updateIndex([state.list[index]], "delete");
+          isFound = true;
+        }
         return {
           list: [...state.list.slice(0, index), ...state.list.slice(index + 1)]
         };
       });
       return isFound;
     },
-    replaceItem: (uid: string, item) => {
+    updateItem: (uid: string, item) => {
       get().removeItem(uid);
       get().addItem({ uid, ...item });
     },
+    updateIndex: (records, type = "add") => {
+
+      // 计算出日期集合
+      /** e.g. { "2021-03-24": 5 } */
+      const dateLengthMap = {};
+      records.forEach(item => {
+        if (!dateLengthMap[item.date])
+          dateLengthMap[item.date] = 0;
+        dateLengthMap[item.date] ++;
+      });
+
+      // 按日期逐个更新
+      Object.keys(dateLengthMap).forEach(date => {
+        // 找到要插入的位置
+        const [YEAR, MONTH] = date
+          .slice(0, date.lastIndexOf("-"))
+          .split("-")
+          .map(item => parseInt(item));
+        set(state => {
+          const total = dateLengthMap[date];
+          let indexList = state.indexList;
+          let index = indexList.findIndex(item => {
+            const [year, month] = item.date.split("-").map(item => parseInt(item));
+            if (year < YEAR) {
+              return true;
+            } else if (year === YEAR && month < MONTH) {
+              return true;
+            }
+          });
+
+          if (index === -1) index = indexList.length;
+          if (indexList[index - 1]?.date !== `${YEAR}-${MONTH}`) {
+            // initialize
+            indexList = [...state.indexList.slice(0, index), {
+              date: `${YEAR}-${MONTH}`,
+              // 取前一个的 尾部地址
+              index: (indexList[index - 1]?.index || 0) + (indexList[index - 1]?.length || 0),
+              length: 0
+            }, ...state.indexList.slice(index)];
+            index++;
+          }
+
+          // 执行修改
+          if (type === "add") {
+            if (index - 1 >= 0) indexList[index - 1].length += total;
+            for (let i = index; i < indexList.length; i++)
+              if (indexList[i].index !== undefined) indexList[i].index! += total;
+          }
+          else {
+            if (index - 1 >= 0) indexList[index - 1].length -= total;
+            for (let i = index; i < indexList.length; i++)
+              if (indexList[i].index !== undefined) indexList[i].index! -= total;
+          }
+          return ({ indexList });
+        });
+      });
+    }
   }),
   {
     name: "billRecords",
